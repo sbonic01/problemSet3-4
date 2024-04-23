e2e99c6409f98a87f9478504b3625f3e489d7c86
sbonic01 <113803459+sbonic01@users.noreply.github.com>
2024-04-22 21:53:34 -0400

    Update search_places.js

--- search_places.js
+++ search_places.js
@@ -18,51 +18,51 @@ res.write("url is: " + path + "<hr>");
   }
   else if (path == "/process")
     res.write ("Processing, the value of place is: " + querystr);
-//     const MongoClient = require('mongodb').MongoClient;
-//     const url = "mongodb+srv://something123:something123@cluster0.swfkiih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+    const MongoClient = require('mongodb').MongoClient;
+    const url = "mongodb+srv://something123:something123@cluster0.swfkiih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-//     MongoClient.connect(url, async function(err, mydb) {
-//         if(err) { console.log(err); }
-//         else {
-//             var dbo = mydb.db("problemSet3-4");
-//             var collection = dbo.collection('places');
+    MongoClient.connect(url, async function(err, mydb) {
+        if(err) { console.log(err); }
+        else {
+            var dbo = mydb.db("problemSet3-4");
+            var collection = dbo.collection('places');
 
-//             // Check if the entry is a zip code or a place
-//             isPlace = true;
-//             if (isNaN(querystr)) {
-//                 isPlace = false;
-//             }
+            // Check if the entry is a zip code or a place
+            isPlace = true;
+            if (isNaN(querystr)) {
+                isPlace = false;
+            }
 
-//             // It sounds like whether they enter a zip code or place we still output the place and all the associated zip codes
-//             // Handle place
-//             if (isPlace) {
-//                 theQuery = {place: querystr};
-//             } else {
-//                 theQuery = {zips: querystr};
-//             }
-//             result = collection.find(theQuery);
+            // It sounds like whether they enter a zip code or place we still output the place and all the associated zip codes
+            // Handle place
+            if (isPlace) {
+                theQuery = {place: querystr};
+            } else {
+                theQuery = {zips: querystr};
+            }
+            result = collection.find(theQuery);
 
-//             // If place is not in db, write some message
-//             if ((await result.count() === 0)) {
-//                 if (isPlace) {res.write('This place is not in our database');}
-//                 else {res.write('This zipcode is not in our database');}
-//             } else {
-//                 result.toArray(function(err, items) {
+            // If place is not in db, write some message
+            if ((await result.count() === 0)) {
+                if (isPlace) {res.write('This place is not in our database');}
+                else {res.write('This zipcode is not in our database');}
+            } else {
+                result.toArray(function(err, items) {
 
-//                     if (err) {
-//                       console.log("Error: " + err);
-//                     } 
-//                     else 
-//                     {
-//                       console.log("Results: ");
-//                       for (i=0; i<items.length; i++)
-//                           console.log(items[i].place + ': ' + items[i].zips);		
-//                     }
-//                 })   
-//             }
-//             mydb.close();
-//         }
-//     })
+                    if (err) {
+                      console.log("Error: " + err);
+                    } 
+                    else 
+                    {
+                      console.log("Results: ");
+                      for (i=0; i<items.length; i++)
+                          console.log(items[i].place + ': ' + items[i].zips);		
+                    }
+                })   
+            }
+            mydb.close();
+        }
+    })
     
 
 
