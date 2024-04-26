3fc0c316818b666a07f26be3f218c5a69ce68cae
sbonic01 <113803459+sbonic01@users.noreply.github.com>
2024-04-25 22:12:05 -0400

    Update search_places.js

--- search_places.js
+++ search_places.js
@@ -1,12 +1,12 @@
 var http = require('http');
 var url = require("url");
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 27017;
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
 var path = url.parse(req.url).pathname;
 var querystr = url.parse(req.url, true).query.place;
-res.write("url is: " + path + "<hr>");
+
   if (path == "/")
   {
     res.write('Please enter the name of a place (city) or zip code.')
@@ -26,48 +26,44 @@ res.write("url is: " + path + "<hr>");
 
     res.write("21");
     const MongoClient = require('mongodb').MongoClient;
-    // const url2 = "mongodb+srv://something123:something123@cluster0.swfkiih.mongodb.net/problemSet3-4?retryWrites=true&w=majority&appName=Cluster0";
-    const url2 = process.env.MONGODB_URI;
+    const url2 = "mongodb+srv://something123:something123@cluster0.swfkiih.mongodb.net/problemSet3-4?retryWrites=true&w=majority&appName=Cluster0";
+    // const url2 = process.env.MONGODB_URI;
     
-    try {
-      MongoClient.connect(url2, async function(err, mydb) {
-          res.write("27");
-          if(err) { console.log(err); }
-          else {
-              var dbo = mydb.db("problemSet3-4");
-              var collection = dbo.collection('places');
-              res.write("In here");
+    MongoClient.connect(url2, async function(err, mydb) {
+        res.write("27");
+        if(err) { console.log(err); }
+        else {
+            var dbo = mydb.db("problemSet3-4");
+            var collection = dbo.collection('places');
+            res.write("In here");
 
-              // Check if the entry is a zip code or a place
-              isPlace = true;
-              if (isNaN(querystr)) {
-                  isPlace = false;
-              }
+            // Check if the entry is a zip code or a place
+            isPlace = true;
+            if (isNaN(querystr)) {
+                isPlace = false;
+            }
 
-              // It sounds like whether they enter a zip code or place we still output the place and all the associated zip codes
-              // Handle place
-              if (isPlace) {
-                  theQuery = {place: querystr};
-              } else {
-                  theQuery = {zips: querystr};
-              }
-              result = collection.find(theQuery);
+            // Whether they enter a zip code or place we still output the place and all the associated zip codes
+            // Handle place
+            if (isPlace) {
+                theQuery = {place: querystr};
+            } else {
+                theQuery = {zips: querystr};
+            }
+            result = await collection.find(theQuery);
 
-              // If place is not in db, write some message
-              if ((await result.count() === 0)) {
-                  if (isPlace) {res.write('This place is not in our database');}
-                  else {res.write('This zipcode is not in our database');}
-              } else {
-                await result.forEach(function(item){
-                  res.write(item.place + ': ' + item.zips);		
-                }) 
-              }
-              mydb.close();
-          }
-      })
-    } catch(error) {
-      console.error(error);
-    }
+            // If place is not in db, write some message
+            if ((await result.count() === 0)) {
+                if (isPlace) {res.write('This place is not in our database');}
+                else {res.write('This zipcode is not in our database');}
+            } else {
+              await result.forEach(function(item){
+                res.write(item.place + ': ' + item.zips);		
+              }) 
+            }
+            mydb.close();
+        }
+    })
 
   }
   res.end();
