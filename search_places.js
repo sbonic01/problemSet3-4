4bd2878ea146645fbdacd929ca63eaaf811c61a1
sbonic01 <113803459+sbonic01@users.noreply.github.com>
2024-04-25 21:12:01 -0400

    Update search_places.js

--- search_places.js
+++ search_places.js
@@ -9,7 +9,7 @@ var querystr = url.parse(req.url, true).query.place;
 res.write("url is: " + path + "<hr>");
   if (path == "/")
   {
-    res.write('Please enter the name of a place (city, state) or zip code.')
+    res.write('Please enter the name of a place (city) or zip code.')
     s = "<form action='/process' method='get'>"+
             "<input type='text' name='place'><br>" +
             "<input type='submit'>" +
@@ -30,50 +30,54 @@ res.write("url is: " + path + "<hr>");
     // const url2 = process.env.MONGODB_URI;
     
     res.write("25");
-    MongoClient.connect(url2, async function(err, mydb) {
-        res.write("27");
-        if(err) { console.log(err); }
-        else {
-            var dbo = mydb.db("problemSet3-4");
-            var collection = dbo.collection('places');
-            res.write("In here");
+    try {
+      MongoClient.connect(url2, async function(err, mydb) {
+          res.write("27");
+          if(err) { console.log(err); }
+          else {
+              var dbo = mydb.db("problemSet3-4");
+              var collection = dbo.collection('places');
+              res.write("In here");
 
-            // // Check if the entry is a zip code or a place
-            // isPlace = true;
-            // if (isNaN(querystr)) {
-            //     isPlace = false;
-            // }
+              // // Check if the entry is a zip code or a place
+              // isPlace = true;
+              // if (isNaN(querystr)) {
+              //     isPlace = false;
+              // }
 
-            // // It sounds like whether they enter a zip code or place we still output the place and all the associated zip codes
-            // // Handle place
-            // if (isPlace) {
-            //     theQuery = {place: querystr};
-            // } else {
-            //     theQuery = {zips: querystr};
-            // }
-            // result = collection.find(theQuery);
+              // // It sounds like whether they enter a zip code or place we still output the place and all the associated zip codes
+              // // Handle place
+              // if (isPlace) {
+              //     theQuery = {place: querystr};
+              // } else {
+              //     theQuery = {zips: querystr};
+              // }
+              // result = collection.find(theQuery);
 
-            // // If place is not in db, write some message
-            // if ((await result.count() === 0)) {
-            //     if (isPlace) {res.write('This place is not in our database');}
-            //     else {res.write('This zipcode is not in our database');}
-            // } else {
-            //     result.toArray(function(err, items) {
+              // // If place is not in db, write some message
+              // if ((await result.count() === 0)) {
+              //     if (isPlace) {res.write('This place is not in our database');}
+              //     else {res.write('This zipcode is not in our database');}
+              // } else {
+              //     result.toArray(function(err, items) {
 
-            //         if (err) {
-            //           res.write("Error: " + err);
-            //         } 
-            //         else 
-            //         {
-            //           res.write("Results: ");
-            //           for (i=0; i<items.length; i++)
-            //               res.write(items[i].place + ': ' + items[i].zips);		
-            //         }
-            //     })   
-            // }
-            mydb.close();
-        }
-    })
+              //         if (err) {
+              //           res.write("Error: " + err);
+              //         } 
+              //         else 
+              //         {
+              //           res.write("Results: ");
+              //           for (i=0; i<items.length; i++)
+              //               res.write(items[i].place + ': ' + items[i].zips);		
+              //         }
+              //     })   
+              // }
+              mydb.close();
+          }
+      })
+    } catch(error) {
+      console.log(error);
+    }
     
     res.write("72");
 
