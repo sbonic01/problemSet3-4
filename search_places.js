8e36cec251222453824d0ce8c3aef92580e1dfb6
sbonic01 <113803459+sbonic01@users.noreply.github.com>
2024-04-28 17:41:38 -0400

    Update search_places.js

--- search_places.js
+++ search_places.js
@@ -15,6 +15,7 @@ var querystr = url.parse(req.url, true).query.place;
             "<input type='submit'>" +
             "</form>";
 	  res.write(s);
+    res.end();
   }
   else if (path == "/process") {
     if (isNaN(querystr)) {
@@ -22,28 +23,22 @@ var querystr = url.parse(req.url, true).query.place;
     } else {
         res.write("You entered the zip code " + querystr);
     }
-    res.write("<br><br>If only MongoDB could connect...");
 
     const MongoClient = require('mongodb').MongoClient;
     const url2 = "mongodb+srv://something123:something123@cluster0.swfkiih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-    // const url2 = process.env.MONGODB_URI;
     
     try {
       MongoClient.connect(url2, async function(err, mydb) {
-          console.log("inside the mongo section");
           if(err) { console.log(err); }
           else {
               var dbo = mydb.db("problemSet3-4");
               var collection = dbo.collection('places');
-              console.log("In the else");
 
               // Check if the entry is a zip code or a place
               isPlace = true;
               if (!isNaN(querystr)) {
                   isPlace = false;
               }
-
-              res.write("line 46");
 
               // Whether they enter a zip code or place we still output the place and all the associated zip codes
               // Handle place
@@ -52,28 +47,26 @@ var querystr = url.parse(req.url, true).query.place;
               } else {
                   theQuery = {'zips': querystr};
               }
-              console.log("place:" + isPlace);
-              console.log("query:" + theQuery);
               result = await collection.find(theQuery);
-              console.log("line 56");
+
               // If place is not in db, write some message
               if ((await result.count() === 0)) {
-                  if (isPlace) {console.log('This place is not in our database');}
-                  else {console.log('This zipcode is not in our database');}
+                  if (isPlace) {res.write('This place is not in our database');}
+                  else {res.write('This zipcode is not in our database');}
               } else {
                 await result.forEach(function(item){
                   res.write(item.place + ': ' + item.zips);		
                 }) 
               }
-              console.log("line 66");
+
               mydb.close();
+              res.end();
           }
       })
     } catch(err) {
       console.log(err);
+      res.end();
     }
-  console.log("after the mongo section");
   }
-  res.end();
 
 }).listen(port);
